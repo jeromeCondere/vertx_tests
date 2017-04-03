e4ded689b33bae673f5552b75db121f5ba16aca1
Condere Jerome <jerome.condere@gmail.com>
2017-04-03 22:25:47 +0200

    service1 begin

--- src/main/java/com/vertxExample/services/Service1.java
+++ src/main/java/com/vertxExample/services/Service1.java
@@ -1,7 +1,19 @@
 package com.vertxExample.services;
 
 import io.vertx.core.AbstractVerticle;
+import io.vertx.core.Future;
 
-public class Service1 extends AbstractVerticle {
-
+public class Service1 extends AbstractVerticle 
+{
+	@Override
+	final public void start() throws Exception 
+	{
+		
+	}
+	
+	@Override
+	final public void stop() throws Exception 
+	{
+		
+	}
 }
