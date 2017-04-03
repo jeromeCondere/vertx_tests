5025d6a00dc6609613cf1edab6cdd24960de9206
Condere Jerome <jerome.condere@gmail.com>
2017-04-03 22:21:00 +0200

    add cluster config

--- src/main/java/com/vertxExample/app/App.java
+++ src/main/java/com/vertxExample/app/App.java
@@ -1,13 +1,29 @@
 package com.vertxExample.app;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import io.vertx.core.DeploymentOptions;
+import io.vertx.core.Vertx;
+import io.vertx.core.VertxOptions;
+import io.vertx.core.spi.cluster.ClusterManager;
+import io.vertx.spi.cluster.hazelcast.HazelcastClusterManager;
+
+public class App  {
+	public static void main(String[] args) {
+   
+        final DeploymentOptions deploymentOptions = new DeploymentOptions()
+                .setInstances(5);
+        
+        final ClusterManager mgr = new HazelcastClusterManager();
+        final VertxOptions options = new VertxOptions().setClusterManager(mgr);
+        												
+        Vertx.clusteredVertx(options, res -> {
+            if (res.succeeded()) {
+                
+                final Vertx vertx = res.result();
+                //vertx.deployVerticle();
+            } else {
+                System.out.println("FAIL !!!");
+            }
+        });
+
     }
 }
---
+++ src/main/java/com/vertxExample/services/Service1.java
@@ -0,0 +1,7 @@
+package com.vertxExample.services;
+
+import io.vertx.core.AbstractVerticle;
+
+public class Service1 extends AbstractVerticle {
+
+}
